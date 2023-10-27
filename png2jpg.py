43bab2cfc85d64c3538b4d7a64006605a09bccf1
salab3rt <salabert57@gmail.com>
2023-10-27 18:26:51 +0100

    -

--- png2jpg.py
+++ png2jpg.py
@@ -1,20 +1,55 @@
-import json
 from pathlib import Path
 from PIL import Image
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import sqlite3
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('backup.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 # Define the paths
 start_dir = Path('./imgs')
 backup_dir = start_dir / 'backup'
 backup_info_file = Path('./backup_info.json')
-log_file = Path('./processed_files.json')  # New log file for tracking processed files
 
 # Create the "backup" folder and directory if they don't exist
 backup_dir.mkdir(parents=True, exist_ok=True)
 
-# Initialize a dictionary to store the backed-up folder names and file names
-backed_up_data = {}
+# Create the database if it doesn't exist
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS processed_files (file_path TEXT PRIMARY KEY)')
+conn.commit()
+
+cur.close()
+conn.close()
+
+# Open the JSON file when the program starts
+def is_file_processed(file_path, conn):
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM processed_files WHERE file_path = ?', (file_path,))
+    row = cur.fetchone()
+    cur.close()
+
+    if row is not None:
+        logger.info(f'Already Processed: {row}')
+
+    return row is not None
+
+
+def save_db_record(file_path, conn):
+    cur = conn.cursor()
+    cur.execute('INSERT INTO processed_files (file_path) VALUES (?)', (file_path.name,))
+    cur.close()
+
 
 # Function to convert and backup a file
 def convert_and_backup(file_path):
@@ -30,56 +65,30 @@ def convert_and_backup(file_path):
         elif source_folder_name.startswith('cobas_6500_'):
             custom_folder_name = 'core_' + source_folder_name[len('cobas_6500_'):]
 
-        # Check if the file has been processed for this folder
-        if not is_file_processed(log_file, custom_folder_name, relative_path):
-            # Open the PNG image
-            with Image.open(file_path) as img:
-                img = img.convert('RGB')
-                new_width = img.width // 2
-                new_height = img.height // 2
-                img = img.resize((new_width, new_height))
+        # Open the PNG image
+        with Image.open(file_path) as img:
+            img = img.convert('RGB')
+            new_width = img.width // 2
+            new_height = img.height // 2
+            img = img.resize((new_width, new_height), Image.Resampling.BILINEAR)
 
-                # Define the destination path with the custom folder name
-                dest_path = backup_dir / custom_folder_name / relative_path.with_suffix('.jpg')
-                dest_path.parent.mkdir(parents=True, exist_ok=True)
-                img.save(dest_path)
+            # Define the destination path with the custom folder name
+            dest_path = backup_dir / custom_folder_name / relative_path.with_suffix('.jpg')
+            dest_path.parent.mkdir(parents=True, exist_ok=True)
+            img.save(dest_path)
 
-            # Log the processed file
-            log_processed_file(log_file, source_folder_name, relative_path)
+        logger.info(f'Converted file: {file_path}')
 
-# Function to check if a file has been processed
-def is_file_processed(log_file, folder_name, relative_path):
-    processed_files = load_processed_files(log_file)
-    return folder_name in processed_files and relative_path in processed_files[folder_name]
-
-# Function to log a processed file
-def log_processed_file(log_file, folder_name, relative_path):
-    processed_files = load_processed_files(log_file)
-    if folder_name in processed_files:
-        processed_files[folder_name].append(relative_path)
-    else:
-        processed_files[folder_name] = [relative_path]
-    save_processed_files(log_file, processed_files)
-
-# Function to load the processed files data
-def load_processed_files(log_file):
-    if not log_file.exists():
-        return {}
-
-    with open(log_file, 'r') as file:
-        return json.load(file)
-
-# Function to save the processed files data
-def save_processed_files(log_file, data):
-    with open(log_file, 'w') as file:
-        json.dump(data, file, default=str)
 
 class FileHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory:
             file_path = Path(event.src_path)
-            if file_path.is_file() and file_path.suffix.lower() == '.png':
-                convert_and_backup(file_path)
+            with sqlite3.connect('database.db') as conn:
+                if file_path.is_file() and file_path.suffix.lower() == '.png' and not is_file_processed(file_path.name, conn):
+                    convert_and_backup(file_path)
+                    logger.info(f'Processed file: {file_path}')
+                    save_db_record(file_path, conn)
 
 if __name__ == "__main__":
     # Start monitoring the "imgs" folder for new files
